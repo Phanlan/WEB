81f319650d5b0fe2c85339195287e16bc2079099
Phanlan <62745201+Phanlan@users.noreply.github.com>
2021-08-19 20:34:40 +0700

    Initial commit

---
+++ JS/locSp.js
@@ -0,0 +1,27 @@
+function changeproducList(type, element) {
+    //console.log(element);
+    let tabs= document.getElementsByClassName('tab-item');
+    for(i=0;i<tabs.length;i++){
+        tabs[i].style.background="#b14c38";
+    }
+    element.style.background = "#ee4d2d";
+    
+    document.getElementById(type).style.display='block';
+    switch(type){
+        case 'trend' :
+       
+            document.getElementById('new').style.display='none';
+            document.getElementById('best-sell').style.display='none';
+            break;
+        case 'new':
+            document.getElementById('trend').style.display='none';
+            document.getElementById('best-sell').style.display='none';
+            break;
+        case 'best-sell':
+            document.getElementById('trend').style.display='none';
+            document.getElementById('new').style.display='none';
+            break;
+
+    }
+}
+
